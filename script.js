3624860789db963456960fd931d5278f1d7d2729
Kah <karina.rezende@escola.pr.gov.br>
2025-10-03 21:52:55 -0300

    salvar

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avança = document.querySelectorAll('.btn-proximo');
 avança.forEach(button => {
     button.addEventListener('click', function () {
         const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-    atual.classList.remove('ativo');
-    document.getElementById(proximoPasso).classList.add('ativo');
-})
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
